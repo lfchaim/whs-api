e010aea2417f483a6dadbc8452ddd684663c0451
fernando <fernando@fernando-Inspiron-15-7000>
2023-02-18 09:17:08 -0300

    Maven added

---
+++ src/main/java/com/whs/whsapi/WhsApiApplication.java
@@ -0,0 +1,13 @@
+package com.whs.whsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/whs/whsapi/WhsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whs.whsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
